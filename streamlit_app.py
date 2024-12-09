d3f43a7b815a7e61ec77390996db82ffb7f14a92
abhi090902 <abhishek.r@buzzboard.com>
2024-12-09 12:04:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ st.title("AI Content Rating Analysis")
 
 # Add input for OpenAI API key
 
-#api_key = st.text_input("Enter your OpenAI API key", type="password")
+api_key = st.text_input("Enter your OpenAI API key", type="password")
 #OPENAI_API_KEY: ${{ secrets.OPENAI_API_KEY }}
-api_key = st.secrets["OPENAI_API_KEY"]
+#api_key = st.secrets["OPENAI_API_KEY"]
 
 if api_key:
     openai.api_key = api_key
